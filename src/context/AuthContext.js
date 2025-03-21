04c37a15978d93bd13a2777696fcc6161b99dbe2
Jacob Krzeminski <jacobkrez40@gmail.com>
2025-03-21 00:18:30 -0400

    Move AuthContext.js to src/context/ directory

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { auth } from '../firebase';
-import {
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-} from 'firebase/auth';
+import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const AuthContext = createContext();
 
@@ -34,7 +29,6 @@ export function AuthProvider({ children }) {
       setCurrentUser(user);
       setLoading(false);
     });
-
     return unsubscribe;
   }, []);
 
